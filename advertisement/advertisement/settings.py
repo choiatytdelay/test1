91f7a5e45b72733c5968303d4a4a3f55598f6f5c
choiatytdelay <alimit0110@gmail.com>
2023-07-24 15:39:11 +0500

    1.1

--- advertisement/advertisement/settings.py
+++ advertisement/advertisement/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'advertisement.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- advertisement/app_advertisement/urls.py
+++ advertisement/app_advertisement/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import fun
+from .views import index, top_sellers
 
 
 urlpatterns = [
-    path('', fun)
+    path('', index, name='main_page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
 ]
